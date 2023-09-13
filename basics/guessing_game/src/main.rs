a616a302fe708678007ebc6608f58843e0441c16
3L3SG <imelesg@outlook.com>
2023-09-13 21:17:47 +0530

    working guessing game

--- basics/guessing_game/src/main.rs
+++ basics/guessing_game/src/main.rs
@@ -1,19 +1,36 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess");
+        println!("\nPlease input your guess : ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read the line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line");
 
-    println!("The secret numbner is: {}", &secret_number);
-    println!("You guessed: {}", &guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("The secret number is: {}", &secret_number);
+        println!("You guessed: {}", &guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Sorry, too small!"),
+            Ordering::Greater => println!("Sorry, too big!"),
+            Ordering::Equal => {
+                println!("Damn, you are right!");
+                break;
+            }
+        };
+    }
 }
